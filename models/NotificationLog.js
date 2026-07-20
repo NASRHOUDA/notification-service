b2fb83be7f60007f0a0b7b67ebdb88301f6616cf
NASRHOUDA <houdanasr414@gmail.com>
2026-07-20 19:18:58 +0100

    feat: add NotificationLog model for notification-db

---
+++ models/NotificationLog.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const NotificationLog = sequelize.define(
+    "NotificationLog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      taskId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM("deadline", "overdue", "reminder"),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      sentAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      status: {
+        type: DataTypes.ENUM("sent", "failed"),
+        defaultValue: "sent",
+      },
+      error: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "notification_logs",
+      timestamps: true,
+    }
+  );
+
+  return NotificationLog;
+};
